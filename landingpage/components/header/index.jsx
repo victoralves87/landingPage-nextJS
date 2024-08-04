3380a5a58023cab466e9221f1763b7443cc74ef6
victor alves <voctorralves@gmail.com>
2024-08-03 22:53:51 -0300

    componente header

---
+++ landingpage/components/header/index.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Logo from "/public/images/logo3.png";
+import Link from "next/link";
+import Styles from "./header.module.scss"
+
+const Header = () => {
+    return(
+        <div className={Styles.container}>
+            <div className={Styles.logotipo}>
+            <Image src={Logo} width={80}  alt="Logo" />
+            </div>
+            <div className={Styles.menu}>
+                <Link href="/">Home</Link>
+                 <Link href="/">O que fazemos?</Link>
+                 <Link href="/">Cases</Link>
+            </div>
+            <div className={Styles.action}>
+                <button className={Styles.button}>Fale conosco</button>
+            </div>
+        </div>
+       
+    )
+}
+
+export default Header;
--- landingpage/pages/index.jsx
+++ landingpage/pages/index.jsx
@@ -1,10 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "./Home.module.scss";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className={styles.container}></div>
+  return <div className={styles.container}>
+    <Header/>
+  </div>
 }
