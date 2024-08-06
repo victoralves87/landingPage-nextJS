d5098d2032713a8bcd114d5a4abc2c2a965ffc2e
victor alves <voctorralves@gmail.com>
2024-08-06 16:18:41 -0300

    iniciando parte features

---
+++ landingpage/components/features/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+        <h1>Neste Bairro, não há Agência Melhor, garantimos!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam minus delectus facilis quas. Tenetur accusamus porro modi deleniti totam id iste. Laborum molestias adipisci esse porro.</p>
+    </div>
+  )
+}
+
+export default Feature
--- landingpage/components/welcome/index.jsx
+++ landingpage/components/welcome/index.jsx
@@ -1,20 +1,26 @@
 import React from "react"
 import Styles from "./welcome.module.scss"
 import Button from "../button"
+import BannerWelcome from "/public/images/midia.jpg"
+import Image from "next/image"
 
 const Welcome = () => {
-  return  <div className={Styles.container}>
-            <div className="Styles.text">
-                <h1>Melhor agência de Marketing do Bairro!</h1>
-                <p>Somos uma agência  de performace digital, aceleramos vendas
-                    e aquisição de leads.</p>
-                <Button title="Aumentar vendas" kind="secondary"/>    
-            </div>
+    return (
+        <div className={Styles.wrapper}>
+            <div className={Styles.container}>
+                <div className={Styles.text}>
+                    <h1>Melhor agência de Marketing do Bairro!</h1>
+                    <p>Somos uma agência de performance digital, aceleramos vendas
+                        e aquisição de leads.</p>
+                    <Button title="Aumentar vendas" kind="secondary"/>    
+                </div>
 
-            <div className="Styles.image">
-
+                <div className={Styles.image}>
+                    <Image src={BannerWelcome} alt="imagem banner"/>
+                </div>
             </div>
         </div>
+    );
 
 }
 
